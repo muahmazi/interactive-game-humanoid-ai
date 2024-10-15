2efb842f24dded7579150552ae66430c5bd7ad3b
a9sk <920a9sk42f76c765@proton.me>
2024-10-15 22:04:23 +0200

    comments

--- src/Drawable.java
+++ src/Drawable.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 
 public interface Drawable {
+    // this does not need many comments i guess
     void draw(Graphics2D g, int viewportX, int viewportY, int windowWidth, int windowHeight);
 }
